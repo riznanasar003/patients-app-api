d71fbbe6dde8e6aed02778ea8f029ed845e212d9
riznanasar003 <richurizna10@gmail.com>
2024-08-06 20:07:43 +0530

    created

---
+++ models/admin.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const loginSchema = mongoose.Schema(
+    {
+        username:{type:String,required:true},
+        password:{type:String,required:true}
+    }
+)
+
+const loginModel = mongoose.model("logindata",loginSchema)
+module.exports = loginModel
